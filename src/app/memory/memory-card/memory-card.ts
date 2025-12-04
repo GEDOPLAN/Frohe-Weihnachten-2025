6b9733008bf3dd5e9e4d50b087fabfe72095d0e8
Dominik Mathmann <dominik.mathmann@gedoplan.de>
2025-12-04 08:17:52 +0100

    Frohe Weihnachten 2025

---
+++ src/app/memory/memory-card/memory-card.ts
@@ -0,0 +1,25 @@
+import {Component, input, output, signal} from '@angular/core';
+import {Card} from "../memory.models";
+import {MatCard} from "@angular/material/card";
+
+@Component({
+  selector: 'gedx2025-memory-card',
+  imports: [
+    MatCard
+  ],
+  templateUrl: './memory-card.html',
+  styleUrl: './memory-card.scss',
+})
+export class MemoryCard {
+
+  card = input.required<Card>();
+  enabled = input<boolean>(true);
+  clicked = output<void>();
+
+
+  protected cardClick() {
+    if(this.enabled()){
+      this.clicked.emit();
+    }
+  }
+}
